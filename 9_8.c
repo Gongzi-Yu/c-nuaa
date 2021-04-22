d408345a0a04c19147eb882ca474e7bf51bf17e2
zhang <1643532623@qq.com>
2021-04-22 15:36:04 +0800

    调整了一下顺序，有些文件命名错误

---
+++ 9_8.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int isLetter(char c)
+{
+	if((c>=65&&c<=90)||(c>=97&&c<=122))
+		return 1;
+	else return 0;
+}
+char toUpper(char c)
+{
+	if(c >= 65&&c<=90)
+		return c;
+	else
+	{
+		int  s = c - 32;
+		return s;
+	}
+}
+void firstUpper(char *s)
+{
+	char *p = s;
+	int count = 0;
+	while(*p!=0)
+	{
+		if(isLetter(*p)&&(count == 0))
+		{
+			*p = toUpper(*p);
+			*p++;
+			count = 1;
+		}
+		if(!isLetter(*p++))
+			count = 0;
+	}
+}
+int main()
+{
+	char s[] = "there are five apples in the basket";
+	firstUpper(s);
+	printf("%s",s);
+}
---
+++ 9_9.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+void filter(char *a)
+{
+	char p[100];
+	int i, j = 0;
+	for (i = 0; i < 100; i++)
+	{
+		if (a[i] >= 65 && a[i] <= 90)
+			p[j++] = a[i];
+		if (a[i] >= 97 && a[i] <= 122)
+			p[j++] = a[i] - 32;
+	}
+	p[j] = 0;
+	for (i = 0; i < 100; i++)
+		a[i] = p[i];
+}
+int palin(char *a)
+{
+	char b[100];
+	filter(b);
+	char* head = b, * tail = b;
+	int count = 1;
+	for (int i = 0; b[i] != 0; i++)
+		tail++;
+	while (head < tail)
+	{
+		if (*head++ != *tail++) {
+			break;
+			count = 0;
+		}
+	}
+	return count;
+}
+int main()
+{
+	char a[100];
+	scanf("%s", a);
+	if (palin(a)) printf("yes");
+	else printf("no");
+	return 0;
+}
